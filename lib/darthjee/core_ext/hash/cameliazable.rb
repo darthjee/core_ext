4ee34551be4a2b3fa71e538175e834ed2e324022
darthjee <darthjee@gmail.com>
2019-04-02 19:10:47 +0200

    Update camelizable documentation

--- lib/darthjee/core_ext/hash/cameliazable.rb
+++ lib/darthjee/core_ext/hash/cameliazable.rb
@@ -3,13 +3,11 @@
 module Darthjee
   module CoreExt
     module Hash
-      # Module holding methods responsible for camelizing
-      # keys of a hash
+      # Module holding methods for camelizing keys of a hash
       #
       # @api public
       module Cameliazable
-        # Change keys to CamelCase without changing the
-        # original hash
+        # Change keys to CamelCase without changing the original hash
         #
         # @return [::Hash] new hash with changed keys
         # @param [::Hash] options options of camelization
@@ -37,8 +35,7 @@ module Darthjee
           dup.camelize_keys!(options)
         end
 
-        # Change keys to CamelCase changing the
-        # original hash
+        # Change keys to CamelCase changing the original hash
         #
         # @return [::Hash] new hash with changed keys
         # @param [::Hash] options options of camelization
@@ -78,10 +75,40 @@ module Darthjee
           camelize_keys!(options)
         end
 
+        # Change all keys to be snakecase
+        #
+        # THis method does not change the original hash
+        #
+        # @param options [::Hash]
+        # @option options recursive [::TrueClass,::FalseClass] flag for recursive transformation
+        #
+        # @see Hash::KeyChanger#change_keys
+        #
+        # @example underscoring all keys
+        #   hash = { firstKey: 1, 'SecondKey' => 2 }
+        #
+        #   hash.underscore_keys  # returns {
+        #                         #   first_key: 1,
+        #                         #   'second_key' => 2
+        #                         # }
+        #
+        # @return [::Hash]
         def underscore_keys(options = {})
           dup.underscore_keys!(options)
         end
 
+        # Change all keys to be snakecase
+        #
+        # THis method changes the original hash
+        #
+        # @param options [::Hash]
+        # @option options recursive [::TrueClass,::FalseClass] flag for recursive transformation
+        #
+        # @see Hash::KeyChanger#change_keys
+        #
+        # @example (see #underscore_keys)
+        #
+        # @return [::Hash]
         def underscore_keys!(options = {})
           Hash::KeyChanger.new(self).underscore_keys(options)
         end
