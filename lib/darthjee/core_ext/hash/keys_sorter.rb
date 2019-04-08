fd0f6774344f29d3ca0f34832fab45e8cdfffef3
darthjee <darthjee@gmail.com>
2019-04-08 19:32:26 +0200

    Add KeysSorter documentation

--- lib/darthjee/core_ext/hash/keys_sorter.rb
+++ lib/darthjee/core_ext/hash/keys_sorter.rb
@@ -3,12 +3,35 @@
 module Darthjee
   module CoreExt
     module Hash
+      # @api private
+      #
+      # @author Darthjee
+      #
+      # Class responsible for sorting keys of a Hash
       class KeysSorter
+        # @param hash [::hash] hash to be sorted
+        # @param recursive [::TrueClass,::FalseClass]
+        #   flag indicating to perform transformation
+        #   recursively
         def initialize(hash, recursive: true)
           @hash = hash
           @recursive = recursive
         end
 
+        # Creates a new Hash sorting it's keys
+        #
+        # @return [::Hash] new hash
+        #
+        # @example (see KeyChangeable#sort_keys)
+        #
+        # @example Simple Usage
+        #   hash = { key: 10, a_key: { z: 5, a: 10 } }
+        #   sorter = Darthjee::CoreExt::Hash::KeysSorter.new(hash)
+        #
+        #   sorter.sort  # returns {
+        #                #   a_key: { a: 10, z: 5 },
+        #                #   key: 10
+        #                # }
         def sort
           {}.tap do |new_hash|
             sorted_keys.each do |key|
@@ -19,10 +42,22 @@ module Darthjee
 
         private
 
+        # @api private
+        # @private
+        #
+        # Returns all keys sorted
+        #
+        # @return [::Array<::Object>]
         def sorted_keys
           hash.keys.sort
         end
 
+        # @api private
+        # @private
+        #
+        # Applies recursion when needed
+        #
+        # @return [::Object]
         def change_value(value)
           return value unless value.is_a?(Hash) && recursive
           value.sort_keys(recursive: true)
---
+++ spec/integration/yard/darthjee/core_ext/hash/keys_sorter_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Darthjee::CoreExt::Hash::KeysSorter do
+  subject(:sorter) { described_class.new(hash) }
+
+  describe 'yard' do
+    describe 'Simple Usage' do
+      let(:hash) { { key: 10, a_key: { z: 5, a: 10 } } }
+
+      it "sorts the keys" do
+        expect(sorter.sort)
+          .to eq(a_key: { a: 10, z: 5 }, key: 10)
+      end
+    end
+  end
+end
