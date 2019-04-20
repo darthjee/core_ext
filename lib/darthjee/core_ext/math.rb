7bbb4cb5ac18ab13ef5f9f152428c9abe9d8f41e
darthjee <darthjee@gmail.com>
2019-04-21 00:13:15 +0200

    Add examples

--- lib/darthjee/core_ext/math.rb
+++ lib/darthjee/core_ext/math.rb
@@ -4,9 +4,23 @@
 module Math
   # Calculates avarage from a series of values
   #
-  # @param values [::Array] list of values
+  # @overload average(values)
+  #   @param values [::Array] list of values
+  #
+  # @overload average(values)
+  #   @param values [::Hash] hash of values and its weights
   #
   # @return [::Float]
+  #
+  # @example Average of array
+  #   values = [1,2,3,4]
+  #
+  #   Math.average(values) # returns 2.5
+  #
+  # @example Average of hash
+  #   values = { 1 => 3, 7 => 1 }
+  #
+  #   Math.average(values) # returns 2.5
   def self.average(values)
     values = values.map { |v| [v, 1] }.to_h unless values.is_a?(Hash)
 
---
+++ spec/integration/yard/math_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+describe Math do
+  describe 'yard' do
+    describe '.average' do
+      describe 'Average of array' do
+        let(:values) { [1,2,3,4] }
+
+        it 'calculates the average' do
+          expect(Math.average(values)).to eq(2.5)
+        end
+      end
+
+      describe 'Average of hash' do
+        let(:values) do
+          {
+            1 => 3,
+            7 => 1
+          }
+        end
+
+        it 'calculates the average' do
+          expect(Math.average(values)).to eq(2.5)
+        end
+      end
+    end
+  end
+end
