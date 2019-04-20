5d8922a8bf09ad11676046214ec553a0a1a772aa
Favini <darthjee@gmail.com>
2019-04-21 00:38:59 +0200

    More documentation (#39)

--- lib/darthjee/core_ext/math.rb
+++ lib/darthjee/core_ext/math.rb
@@ -1,11 +1,34 @@
 # frozen_string_literal: true
 
+# @api public
 module Math
+  # Calculates avarage from a series of values
+  #
+  # @overload average(values)
+  #   @param values [::Array] list of values
+  #
+  # @overload average(values)
+  #   @param values [::Hash] hash of values and its weights
+  #
+  # @return [::Float]
+  #
+  # @example Average of array
+  #   values = [1,2,3,4]
+  #
+  #   Math.average(values) # returns 2.5
+  #
+  # @example Average of hash
+  #   values = { 1 => 3, 7 => 1 }
+  #
+  #   Math.average(values) # returns 2.5
   def self.average(values)
-    values = values.map { |v| [v, 1] }.to_h unless values.is_a?(Hash)
+    if values.is_a?(Hash)
+      weight_sum = values.values.sum
+      values = values.map { |value, weight| value * weight }
+    else
+      weight_sum = values.length
+    end
 
-    values.inject(0) do |sum, vals|
-      sum + vals.inject { |a, b| a * b }
-    end / values.values.sum.to_f
+    values.sum / weight_sum.to_f
   end
 end
--- lib/darthjee/core_ext/object.rb
+++ lib/darthjee/core_ext/object.rb
@@ -2,7 +2,21 @@
 
 module Darthjee
   module CoreExt
+    # @api public
     module Object
+      # Checks if object is any of the given classes
+      #
+      # @param classes [::Array<::Class>] classes to be checked
+      #   against
+      #
+      # @return [::TrueClass,::FalseClass]
+      #
+      # @example
+      #   object = [1, 2, 3]
+      #
+      #   object.is_any?(Hash,  Class)      # false
+      #   object.is_any?(Hash,  Array)      # true
+      #   object.is_any?(Enumerable, Class) # true
       def is_any?(*classes)
         classes.any? do |clazz|
           is_a?(clazz)
