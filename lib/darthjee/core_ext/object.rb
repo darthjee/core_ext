86919a5eb70c549f4d150caf9ebf8ee0b6bb59b0
darthjee <darthjee@gmail.com>
2019-04-21 00:27:59 +0200

    Add documentation to Object#is_any?

--- lib/darthjee/core_ext/object.rb
+++ lib/darthjee/core_ext/object.rb
@@ -2,7 +2,14 @@
 
 module Darthjee
   module CoreExt
+    # @api public
     module Object
+      # Checks if object is any of the given classes
+      #
+      # @param classes [::Array<::Class>] classes to be checked
+      #   against
+      #
+      # @return [::TrueClass,::FalseClass]
       def is_any?(*classes)
         classes.any? do |clazz|
           is_a?(clazz)
