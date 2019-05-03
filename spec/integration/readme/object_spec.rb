b691ec346e26ccaecf5e5e636d45ecd741532fbd
darthjee <darthjee@gmail.com>
2019-05-03 19:48:27 +0200

    Rubocop fixes

--- spec/integration/readme/object_spec.rb
+++ spec/integration/readme/object_spec.rb
@@ -4,27 +4,28 @@ require 'spec_helper'
 
 describe Object do
   describe 'readme' do
+    # rubocop: disable RSpec/PredicateMatcher
     describe '#is_any' do
       context 'when is one of the classes' do
         it do
-          expect(1).to be_is_any(String, Symbol, Numeric)
+          expect(1.is_any?(String, Symbol, Numeric)).to be_truthy
         end
       end
 
       context 'when is none of the classes' do
         it do
-          expect(1).not_to be_is_any(String, Symbol)
+          expect(1.is_any?(String, Symbol)).to be_falsey
         end
       end
     end
 
     describe '#trueful?' do
       context 'when it is a regular object' do
-        it { expect(Object.new.trueful?).to be_truthy }
+        it { expect(described_class.new.trueful?).to be_truthy }
       end
 
       context 'when it is an empty string' do
-        it { expect("".trueful?).to be_truthy }
+        it { expect(''.trueful?).to be_truthy }
       end
 
       context 'when it is an empty hash' do
@@ -43,5 +44,6 @@ describe Object do
         it { expect(nil.trueful?).to be_falsey }
       end
     end
+    # rubocop: enable RSpec/PredicateMatcher
   end
 end
