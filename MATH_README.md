58cbbc346b2ad74bf493d34bf50e117b93ff8ac8
darthjee <darthjee@gmail.com>
2019-05-03 19:21:11 +0200

    Update documentation to math

--- MATH_README.md
+++ MATH_README.md
@@ -4,12 +4,9 @@
 calculates the average of the given values
 
 ```ruby
-Math.average([ 0, 1, 2, 3, 4 ])
-```
+Math.average([0, 1, 2, 3, 4, 5])
 
-returns
-```
-2
+# returns 2.5
 ```
 
 It can also be used to calculate the average of weighted values
@@ -20,11 +17,8 @@ Math.average(
   1 => 2,
   2 => 3,
   3 => 4,
-  4 => 5
+  4 => 6
 )
-```
 
-returns
-```
-2.6666
+# returns 2.6666
 ```
--- spec/integration/readme/math_spec.rb
+++ spec/integration/readme/math_spec.rb
@@ -5,10 +5,26 @@ require 'spec_helper'
 describe Math do
   describe 'readme' do
     describe '.average' do
-      let(:values) { [ 0, 1, 2, 3, 4, 5 ] }
+      let(:values) { [0, 1, 2, 3, 4, 5] }
 
       it 'calculates average' do
         expect(described_class.average(values)).to eq(2.5)
+      end
+
+      context 'when giving a map' do
+        let(:values) do
+          {
+            0 => 1,
+            1 => 2,
+            2 => 3,
+            3 => 4,
+            4 => 5
+          }
+        end
+
+        it 'calculates average' do
+          expect(described_class.average(values)).to eq(2.6666666666666666)
+        end
       end
     end
   end
